e79914fb915047c77950a44f8695e07f6861f32e
Sheraz Hussain <sherazhussainofficial1@gmail.com>
2025-08-11 15:21:49 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
-  projectId: "sync-tech-solutions",
-  appId: "1:139765189784:web:2fefbd7382a1d0792654ed",
-  storageBucket: "sync-tech-solutions.firebasestorage.app",
-  apiKey: "AIzaSyAuSiS5_OXzXf7xnefG1beF598ctA6X3Y4",
-  authDomain: "sync-tech-solutions.firebaseapp.com",
-  messagingSenderId: "139765189784",
+  "projectId": "sync-tech-solutions",
+  "appId": "1:139765189784:web:2fefbd7382a1d0792654ed",
+  "storageBucket": "sync-tech-solutions.firebasestorage.app",
+  "apiKey": "AIzaSyAuSiS5_OXzXf7xnefG1beF598ctA6X3Y4",
+  "authDomain": "sync-tech-solutions.firebaseapp.com",
+  "messagingSenderId": "139765189784",
 };
 
 function initializeFirebase() {
@@ -18,8 +18,8 @@ function initializeFirebase() {
     const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
     if (process.env.NODE_ENV !== 'development') {
-        const appCheck = initializeAppCheck(app, {
-            provider: new ReCaptchaV3Provider('6Ld-8wsqAAAAAIH9-2yL0_9c5j_Y8f7q_8Kx-3gO'), // Replace with your reCAPTCHA v3 site key
+        initializeAppCheck(app, {
+            provider: new ReCaptchaV3Provider('6Ld-8wsqAAAAAIH9-2yL0_9c5j_Y8f7q_8Kx-3gO'), // Public reCAPTCHA v3 site key
             isTokenAutoRefreshEnabled: true,
         });
     }

44a1136a1ecf716f1ac4dcd69f27b451ef75973c
Sheraz Hussain <sherazhussainofficial1@gmail.com>
2025-08-23 15:17:11 +0000

    i am setting up google tagmanager account can we do these pleae "<!-- Go

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,8 +96,21 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N63V26QZ');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={cn('font-sans antialiased min-h-screen flex flex-col')}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N63V26QZ"
+          height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Script 
             type="text/javascript" 
             src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" 

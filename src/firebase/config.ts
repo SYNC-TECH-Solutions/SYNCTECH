c2600ede09228e19cff244a1dd76706f5bef7d6d
Sheraz Hussain <sherazhussainofficial1@gmail.com>
2025-10-04 18:24:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "sync-tech-solutions",
+  "appId": "1:139765189784:web:2fefbd7382a1d0792654ed",
+  "apiKey": "AIzaSyAuSiS5_OXzXf7xnefG1beF598ctA6X3Y4",
+  "authDomain": "sync-tech-solutions.firebaseapp.com",
+  "measurementId": "G-5F3TEBLMZ0",
+  "messagingSenderId": "139765189784"
+};

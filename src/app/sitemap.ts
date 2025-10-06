4136ff44a5a88a4f18562f86863c5f3d7234da86
Sheraz Hussain <sherazhussainofficial1@gmail.com>
2025-10-06 22:43:04 +0000

    lets update the sitemap.xml and make it better with our updated links

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -74,11 +74,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+     {
+      url: `${baseUrl}/testimonials`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
     {
       url: `${baseUrl}/become-a-partner`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
+    },
+     {
+      url: `${baseUrl}/presentation`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/guidelines-synctech`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+     {
+      url: `${baseUrl}/SherazHussain_SYNCTECH`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
     },
     {
       url: `${baseUrl}/privacy-policy`,
